eab19725c3470ad6ffa9590bcdd1db37d032f254
Stefan Mansson <stefan.aotik@gmail.com>
2019-06-26 21:34:42 +0300

    refactor: test deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 export default () => (
     <div>
+        <h4>Add meeeeee</h4>
         <a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=662900450978.673844679700&redirect_uri=https://maddy.cloud/hooks/callback">
             <img
                 alt="Add to Slack"
